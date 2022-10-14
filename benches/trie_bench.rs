531effbc4717b9c8a55f9268d10eab0ca3ca825f
maysonshen <maysonshen@tencent.com>
2022-10-14 16:37:37 +0800

    add trie_bench benchmarks

--- benches/trie_bench.rs
+++ benches/trie_bench.rs
@@ -1,20 +1,28 @@
-// extern crate test;
-//
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use test::Bencher;
-//     use ik_rs::dict::trie::Trie;
-//
-//     #[bench]
-//     fn benchmark_trie(b: &mut Bencher) {
-//         let mut trie = Trie::new();
-//         trie.insert("Test");
-//         trie.insert("Tea");
-//         trie.insert("Background");
-//         trie.insert("Back");
-//         trie.insert("Brown");
-//         b.iter(||trie.match_word("Back"));
-//     }
-//
-// }
+
+use criterion::*;
+use ik_rs::dict::trie::Trie;
+
+fn trie_build() -> Trie {
+    let mut trie = Trie::new();
+    trie.insert("Test");
+    trie.insert("Tea");
+    trie.insert("Background");
+    trie.insert("Back");
+    trie.insert("Brown");
+    trie
+}
+
+fn trie_match() {
+    let mut  trie = trie_build();
+    trie.match_word("Back");
+    trie.match_word("Tea");
+}
+
+fn trie_benchmark(c: &mut Criterion) {
+    c.bench_function("trie match", |b| {
+        b.iter(||trie_match())
+    });
+}
+
+criterion_group!(benches, trie_benchmark);
+criterion_main!(benches);
